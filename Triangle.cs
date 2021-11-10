8e315aa4606e203b7c4b33f0f5902943e3027f82
Opilane TTHK <opilane@tthk.ee>
2021-11-10 13:59:01 +0200

    Add project files.

---
+++ Triangle.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle_Rimitsen
+{
+    class Triangle
+    {
+        public double a;// первая сторона
+        public double b;// вторая сторона
+        public double c;// третья сторона
+
+        public Triangle(double A, double B, double C)// Конструктор
+        {
+            a = A;// создаем с заданными длинами сторон согласно заданию
+            b = B;
+            c = C;
+        }
+        public string outputA()
+        {
+            return Convert.ToString(a);
+        }
+        public string outputB()
+        {
+            return Convert.ToString(b);
+        }
+        public string outputC()
+        {
+            return Convert.ToString(c);
+        }
+        public double Perimeter()
+        {
+            double p = 0;
+            p = a + b + c;
+            return p;
+        }
+        public double Surface()
+        {
+            double s = 0;
+            double p = 0;
+            p = (a + b + c) / 2;
+            s = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            return s;
+        }
+
+        public double GetSetA
+        {
+            get
+            { return a; }
+            set
+            { a = value; }
+        }
+
+        public double GetSetB
+        {
+            get
+            { return b; }
+            set
+            { b = value; }
+        }
+
+        public double GetSetC
+        {
+            get
+            { return c; }
+            set
+            { c = value; }
+        }
+
+        public bool ExistTriange
+        {
+            get
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b))
+                    return false;
+                else return true;
+            }
+        }
+    }
+}
